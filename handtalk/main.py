7ae3d08541178a00f50add6bb70845977a861e5b
Duaa <dooa.alhaddad2002@gmail.com>
2025-03-19 23:44:54 +0300

    HandTalk: added keypoint extraction - Day 3

--- handtalk/main.py
+++ handtalk/main.py
@@ -86,3 +86,23 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 # * Release resources
 cap.release()
 cv2.destroyAllWindows()
+
+pose = []
+for res in results.pose_landmarks.landmark:
+     test =np.array([res.x, res.y, res.z, res.visibility])
+     pose.append(test)
+
+# * Extract keypoints (pose, face, hands) from results
+def extract_keypoints(results):
+    """
+    Extracts keypoint coordinates from MediaPipe results for pose, face, and hands.
+
+    Returns:
+    - A flattened NumPy array of coordinates for each body part.
+    - If no detection is found, returns an array of zeros.
+
+    """
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
